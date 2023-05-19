eacd449d765cb5c3f3eef546dabf74cd82bf6676
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-18 23:40:53 -0500

    POJOs

---
+++ spotifyfmcamel/data/pojos/LastFMTrack.java
@@ -0,0 +1,8 @@
+/**
+ * LastFMTrack.java
+ * This class represents the JSON structures present in the lastFMTrackListing.json file.
+ */
+package spotifyfmcamel.data.pojos;
+public class LastFMTrack {
+
+}
---
+++ spotifyfmcamel/data/pojos/SpotifyAudioFeatures.java
@@ -0,0 +1,9 @@
+/**
+ * SpotifyAudioFeatures.java
+ * This class represents the JSON structures present in the spotifyIDToAudioFeatures.json file.
+ */
+package spotifyfmcamel.data.pojos;
+
+public class SpotifyAudioFeatures {
+
+}
