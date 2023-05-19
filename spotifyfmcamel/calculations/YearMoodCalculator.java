2019a8ee9ea561b448b20968303517a717b99bf9
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-18 23:40:25 -0500

    Strategies

---
+++ spotifyfmcamel/calculations/YearMoodCalculator.java
@@ -0,0 +1,9 @@
+/**
+ * YearMoodCalculator.java
+ * This class will calculate the average mood, valency score, for a given year.
+ */
+package spotifyfmcamel.calculations;
+
+public class YearMoodCalculator implements MoodCalculator {
+
+}
