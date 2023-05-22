ccf2ff8c77716b83d74071ed051f577901995ce1
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-22 10:33:58 -0500

    Update replace_words.sh

--- data/scripts/replace_words.sh
+++ data/scripts/replace_words.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# This script uses the `sed` command to change the title-cased JSON keys into
+# their respective camel-cased equivalents for all *.json files within its respective
+# directory.
+# This was done because the original JSON keys for the LastFM objects were created
+# in title-case, and all of the SpotifyFM messages were in camelcase, but this
+# created problems regarding the serialization and deserialization of JSON objects
+# between the Processor steps.
+
+# Usage
+# ./replace_words.sh
+
 sed -i  ''  's/\"Duration\"/\"duration\"/g' *.json
 sed -i  ''  's/\"Artist\"/\"artist\"/g' *.json
 sed -i  ''  's/\"AlbumName\"/\"albumName\"/g' *.json
