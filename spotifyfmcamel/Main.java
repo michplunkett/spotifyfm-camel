786e69e0720665c3f121af8a9f486e98660c2a1b
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-22 11:32:58 -0500

    Update Main.java

--- spotifyfmcamel/Main.java
+++ spotifyfmcamel/Main.java
@@ -3,7 +3,13 @@ package spotifyfmcamel;
 import spotifyfmcamel.containers.SpotifyFMRunner;
 
 public class Main {
-  public static void main(String args[]) throws Exception {
-    new SpotifyFMRunner();
+  public static void main(String[] args) throws Exception {
+    try {
+      new SpotifyFMRunner();
+    } catch (Exception e) {
+      System.out.println(
+          "The application ran into an error, please check your configuration to make sure everything is setup correctly.");
+      System.out.println("This is the error that you received: " + e.getMessage());
+    }
   }
 }
