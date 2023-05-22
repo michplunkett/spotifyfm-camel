87219354473187a6f62335f1d0a66f5924580680
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-22 03:43:56 -0500

    Create LastFMTrackListingHandler.java

---
+++ spotifyfmcamel/data/LastFMTrackListingHandler.java
@@ -0,0 +1,28 @@
+/** This class is the Singleton that will hold all processed SpotifyFMMessages. */
+package spotifyfmcamel.data;
+
+import java.util.ArrayList;
+import spotifyfmcamel.messages.SpotifyFMMessage;
+
+public class LastFMTrackListingHandler {
+  private static LastFMTrackListingHandler instance = null;
+
+  private ArrayList<SpotifyFMMessage> trackList;
+
+  private LastFMTrackListingHandler() {
+    trackList = new ArrayList<>();
+  }
+
+  public static synchronized LastFMTrackListingHandler getInstance() {
+    if (instance == null) {
+      return instance = new LastFMTrackListingHandler();
+    }
+
+    return instance;
+  }
+
+  public void addFullyQualifiedTrack(SpotifyFMMessage m) {
+    m.addToHistory(this.getClass().getName());
+    trackList.add(m);
+  }
+}
