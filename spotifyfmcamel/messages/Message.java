2064020d63c44d7aba63f994a016390a75e32f5a
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-22 13:58:48 -0500

    Update Message.java

--- spotifyfmcamel/messages/Message.java
+++ spotifyfmcamel/messages/Message.java
@@ -11,10 +11,12 @@ public abstract class Message {
   @JsonProperty("history")
   ArrayList<String> history = new ArrayList<>();
 
+  // Function to add to Message History, the EIP concept.
   public void addToHistory(String className) {
     history.add(className);
   }
 
+  // Function for printing the Message History.
   public void printHistory() {
     System.out.println("This message has been handled by these classes:");
     System.out.println(String.join(", ", history));
