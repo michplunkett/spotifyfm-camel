d6c566a1a9ffe21f37be6620fbe89af400b34e2b
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-22 03:44:40 -0500

    Update SpotifyFMMessage.java

--- spotifyfmcamel/messages/SpotifyFMMessage.java
+++ spotifyfmcamel/messages/SpotifyFMMessage.java
@@ -4,10 +4,10 @@
  */
 package spotifyfmcamel.messages;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -23,25 +23,10 @@ import java.time.format.DateTimeFormatter;
   "playCount",
   "rank",
   "spotifyID",
-  "acousticness",
-  "analysis_url",
-  "danceability",
-  "duration_ms",
-  "energy",
-  "id",
-  "instrumentalness",
-  "key",
-  "liveness",
-  "loudness",
-  "mode",
-  "speechiness",
-  "tempo",
-  "time_signature",
-  "track_href",
-  "uri",
   "valence"
 })
-public class SpotifyFMMessage extends Message implements Serializable {
+@JsonIgnoreProperties(value = { "listenDateTime" })
+public class SpotifyFMMessage extends Message {
   DateTimeFormatter localDateTimeFormatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 
   @JsonProperty("albumName")
@@ -59,8 +44,6 @@ public class SpotifyFMMessage extends Message implements Serializable {
   @JsonProperty("listenDate")
   private String listenDate;
 
-  private LocalDateTime listenDateTime;
-
   @JsonProperty("lowerCaseArtist")
   private String lowerCaseArtist;
 
@@ -76,52 +59,7 @@ public class SpotifyFMMessage extends Message implements Serializable {
   @JsonProperty("spotifyID")
   private String spotifyID;
 
-  @JsonProperty("acousticness")
-  private float acousticness;
-
-  @JsonProperty("analysisUrl")
-  private String analysisUrl;
-
-  @JsonProperty("danceability")
-  private float danceability;
-
-  @JsonProperty("durationMs")
-  private int durationMs;
-
-  @JsonProperty("energy")
-  private float energy;
-
-  @JsonProperty("instrumentalness")
-  private float instrumentalness;
-
-  @JsonProperty("key")
-  private int key;
-
-  @JsonProperty("liveness")
-  private float liveness;
-
-  @JsonProperty("loudness")
-  private float loudness;
-
-  @JsonProperty("mode")
-  private int mode;
-
-  @JsonProperty("speechiness")
-  private float speechiness;
-
-  @JsonProperty("tempo")
-  private float tempo;
-
-  @JsonProperty("timeSignature")
-  private int timeSignature;
-
-  @JsonProperty("trackHref")
-  private String trackHref;
-
-  @JsonProperty("uri")
-  private String uri;
-
-  @JsonProperty("albumName")
+  @JsonProperty("valence")
   private float valence;
 
   public String getAlbumName() {
@@ -133,11 +71,7 @@ public class SpotifyFMMessage extends Message implements Serializable {
   }
 
   public LocalDateTime getListenDateTime() {
-    if (listenDateTime == null) {
-      listenDateTime = LocalDateTime.parse(listenDate, localDateTimeFormatter);
-    }
-
-    return listenDateTime;
+    return LocalDateTime.parse(listenDate, localDateTimeFormatter);
   }
 
   public String getName() {
@@ -154,5 +88,9 @@ public class SpotifyFMMessage extends Message implements Serializable {
 
   public float getValence() {
     return valence;
+  }
+
+  public void setValence(float valence) {
+    this.valence = valence;
   }
 }
