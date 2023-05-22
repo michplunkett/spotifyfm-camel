efd03b612747413c9a9e0f8fd16d43fc358799b8
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-22 12:41:07 -0500

    Update SpotifyFMRouteContainer.java

--- spotifyfmcamel/containers/SpotifyFMRouteContainer.java
+++ spotifyfmcamel/containers/SpotifyFMRouteContainer.java
@@ -13,13 +13,14 @@ import spotifyfmcamel.processors.FullyQualifiedTrackProcessor;
 import spotifyfmcamel.processors.SpotifySearchToIDEnricher;
 
 public class SpotifyFMRouteContainer extends RouteContainer {
-
+  // The strings are static and final because they are used multiple times and are essentially
+  // configuration values.
   static final String brokerURL = "tcp://localhost:61616";
   static final String songQueue = "jms:queue:SPOTIFYFM_SONGSWITHINFO";
   static final String deadLetterQueue = "jms:queue:SPOTIFYFM_DEADLETTER";
-  CamelContext context;
+  private CamelContext context;
 
-  ConnectionFactory connectionFactory;
+  private ConnectionFactory connectionFactory;
 
   public void setUpConnection() throws Exception {
     // create CamelContext
@@ -27,6 +28,7 @@ public class SpotifyFMRouteContainer extends RouteContainer {
 
     // connect to ActiveMQ JMS broker listening on localhost:61616
     connectionFactory = new ActiveMQConnectionFactory(brokerURL);
+    // Add the JMS component.
     context.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
 
     context.addRoutes(
@@ -34,25 +36,29 @@ public class SpotifyFMRouteContainer extends RouteContainer {
           public void configure() {
             // Read all files from the data/input folder
             from("file:data/input?noop=true")
+                // Create the error handler and send any errored messages to the DeadLetter queue.
                 .errorHandler(
+                    // This is an implementation of the DeadLetter Channel EIP concept.
                     deadLetterChannel(deadLetterQueue)
                         .log("This message is being sent to the DeadLetter topic: ${body}"))
                 .unmarshal()
                 .json(JsonLibrary.Jackson)
+                // Use the SpotifySearchToIDEnricher, which is an implementation of the
+                // ContentEnricher EIP concept.
                 .process(new SpotifySearchToIDEnricher())
+                //  Use the AudioFeatureEnricher, which is an implementation of the ContentEnricher
+                // EIP concept.
                 .process(new AudioFeatureEnricher())
                 .to(songQueue);
+            // Create a Selective Consumer that only takes in messages with non-empty SpotifyIDs and
+            // greater than 0.0 valence scores.
             from(songQueue)
                 .unmarshal()
                 .json(JsonLibrary.Jackson)
-                .choice()
-                .when(body().contains("\"spotifyID\":\"\""))
-                .to(deadLetterQueue)
-                .when(body().contains("\"valence\":0.0"))
-                .to(deadLetterQueue)
-                .otherwise()
-                .process(new FullyQualifiedTrackProcessor())
-                .endChoice();
+                .filter(body().regex(".*((?!\"spotifyID\":\"\"|\"valence\":0.0).).*"))
+                .process(new FullyQualifiedTrackProcessor());
+            // Create a DeadLetter Channel consumer, which is an implementation of the Event-driven
+            // Consumer EIP concept.
             from(deadLetterQueue)
                 .marshal()
                 .json(JsonLibrary.Jackson)
