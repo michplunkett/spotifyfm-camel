d7885c91402356596248327ad31a5e604b621a6d
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-22 14:30:45 -0500

    Update DataHandler.java

--- spotifyfmcamel/data/DataHandler.java
+++ spotifyfmcamel/data/DataHandler.java
@@ -1,5 +1,5 @@
 /**
- * This abstract class serves as the Template for the Singleton classes that will handle the JSON
+ * This abstract class serves as the baseline for the Singleton classes that will handle the JSON
  * files that will act as the data stores for this application.
  */
 package spotifyfmcamel.data;
