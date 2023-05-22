11351ca8208ddd7a59f4322291978d229e2e5ff5
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-22 01:29:24 -0500

    Create Runner.java

---
+++ spotifyfmcamel/facades/Runner.java
@@ -0,0 +1,12 @@
+/** This abstract class serves as the basis for the top level of the Facade. */
+package spotifyfmcamel.facades;
+
+public abstract class Runner {
+  abstract void getDataStores();
+
+  abstract void setupRoutes() throws Exception;
+
+  abstract void runMessengerSystem();
+
+  abstract void calculateYearlyMoods();
+}
